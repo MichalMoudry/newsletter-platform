f369e460c0a4f9f7f69537c49baebb88822e13d2
tomaaspb <125911488+tomaaspb@users.noreply.github.com>
2023-05-22 20:09:03 +0200

    Add files via upload
    oprava modelu (odstranění přebytečných čárek + přejmenování post_name na post_title dle tabulky)

---
+++ model/domain.go
@@ -0,0 +1,101 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Newsletter struct {
+	Nltr_ID                uuid.UUID `db:"Nltr_ID"`
+	Nltr_Name              string    `db:"Nltr_Name"`
+	Nltr_Description       string    `db:"Nltr_Description"`
+	Nltr_Inserted_Datetime time.Time `db:"Nltr_Inserted_Datetime"`
+	Nltr_Updated_Datetime  time.Time `db:"Nltr_Updated_Datetime"`
+	Nltr_Author            uuid.UUID `db:"Nltr_Author"`
+	ConcurrencyStamp       uuid.UUID `db:"concurrency_stamp"`
+}
+
+func NewNewsletter(Nltr_Name, Nltr_Description string, Nltr_Author uuid.UUID) *Newsletter {
+	now := time.Now()
+	return &Newsletter{
+		Nltr_ID:                uuid.New(),
+		Nltr_Name:              Nltr_Name,
+		Nltr_Description:       Nltr_Description,
+		Nltr_Inserted_Datetime: now,
+		Nltr_Updated_Datetime:  now,
+		Nltr_Author:            Nltr_Author,
+		ConcurrencyStamp:       uuid.New(),
+	}
+}
+
+type Post struct {
+	Post_ID                uuid.UUID `db:"Post_ID"`
+	Post_Title             string    `db:"Post_Title"`
+	Post_Content           string    `db:"Post_Content"`
+	Post_Publishing_Date   time.Time `db:"Post_Inserted_Datetime"`
+	Post_Inserted_Datetime time.Time `db:"Post_Inserted_Datetime"`
+	Post_Updated_Datetime  time.Time `db:"Post_Updated_Datetime"`
+	Post_Author            uuid.UUID `db:"Post_Author"`
+	Nltr_ID                uuid.UUID `db:"Nltr_ID"`
+	concurrency_stamp      uuid.UUID `db:"concurrency_stamp"`
+}
+
+func NewPost(Post_Title, Post_Content string, Post_Author, Nltr_ID uuid.UUID) *Post {
+	now := time.Now()
+	return &Post{
+		Post_ID:                uuid.New(),
+		Post_Title:             Post_Title,
+		Post_Content:           Post_Content,
+		Post_Publishing_Date:   now,
+		Post_Inserted_Datetime: now,
+		Post_Updated_Datetime:  now,
+		Post_Author:            Post_Author,
+		Nltr_ID:                Nltr_ID,
+		concurrency_stamp:      uuid.New(),
+	}
+}
+
+type Role string
+
+type User struct {
+	Id               uuid.UUID `db:"id"`
+	Email            string    `db:"email"`
+	UserName         string    `db:"user_name"`
+	UserRole         Role      `db:"user_role"`
+	PasswordHash     string    `db:"password_hash"`
+	DateAdded        time.Time `db:"date_added"`
+	DateUpdated      time.Time `db:"date_updated"`
+	ConcurrencyStamp uuid.UUID `db:"concurrency_stamp"`
+}
+
+func NewUser(email, userName, password string) *User {
+	now := time.Now()
+	return &User{
+		Id:               uuid.New(),
+		Email:            email,
+		UserName:         userName,
+		UserRole:         "Editor",
+		PasswordHash:     password,
+		DateAdded:        now,
+		DateUpdated:      now,
+		ConcurrencyStamp: uuid.New(),
+	}
+}
+
+type PasswordResetToken struct {
+	Id             uuid.UUID `db:"id"`
+	ExpirationDate time.Time `db:"expiration_date"`
+	DateAdded      time.Time `db:"date_added"`
+	Email          string    `db:"email"`
+}
+
+func NewPasswordResetToken(email string) *PasswordResetToken {
+	now := time.Now()
+	return &PasswordResetToken{
+		Id:             uuid.New(),
+		ExpirationDate: now.Add(time.Hour * 24),
+		DateAdded:      now,
+		Email:          email,
+	}
+}
